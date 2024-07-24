173560c444d97abbecfcfd4d17415ecf0121a14c
Isabela Crestane <isabelacrestane@gmail.com>
2024-07-24 19:48:36 -0300

    ajustes pequenos (ainda não abre a login)

--- reserva_app/app.py
+++ reserva_app/app.py
@@ -1,52 +1,45 @@
-from Flask import flask, render_template, request
+from flask import Flask, render_template, request
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-#define dicionario de logins
-login_dict = [
+# Define dicionário de logins
+login_dict = []
 
-]
 
 def pegar_login_armazenado():
-    #sldaps
-    print('pegando dados')
-    #login_dict.append(exemplo)
+    # sldaps
+    print('Pegando dados')
+    # login_dict.append(exemplo)
 
 @app.route('/')
-def home():
+def login():
     return render_template('login.html')
 
-@app.route('/',methods=['post'])
-def home():
+@app.route('/reservas', methods=['POST'])
+def reservas():
     return render_template('reservas.html')
 
+# @app.route('/cadastro')
+# def cadastro():
+#     return render_template('cadastro.html')
 
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro.html')
-
-
-#pega os dados do método post no html do template
-@app.route('/cadastro', methods=['post'])
+# Pega os dados do método POST no HTML do template
+@app.route('/cadastro', methods=['POST'])
 def cadastro():
     nome = request.form['nome'] 
     email = request.form['email']
     senha = request.form['senha']
     cad = {
-        'nome':nome ,
-        'email':email,
-        'senha':senha
+        'nome': nome,
+        'email': email,
+        'senha': senha
     }
     login_dict.append(cad)
-    return render_template(cad)
+    return render_template('cadastro.html', cad=cad)  # Passa 'cad' para o template
 
 @app.route('/cadastrar-sala')
 def cadastrar_sala(): 
     return render_template('cadastrar-sala.html')
-
-@app.route('/reservas')
-def reservas():
-    return render_template('reservas.html')
 
 @app.route('/reservar-sala')
 def reservar_sala():
