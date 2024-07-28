0f6a4234612d3df7176cd3c536b127301a2b37fb
Isabella <isabellapantolfo1101@gmail.com>
2024-07-28 20:34:50 -0300

    fix

--- reserva_app/app.py
+++ reserva_app/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for 
 import csv
 
-#passando .csv para acessivel á python
+#csv acessivel a python
 cadastro_csv = 'cad_usuarios.csv'
 cadastro_sala_csv = 'cad_sala.csv'
 
 app = Flask(__name__)
 
-# FUNÇÕES
 
 # salvar dados digitados pelo usuario no csv
 def salvar_cadastro(nome, email, senha):
@@ -17,11 +16,11 @@ def salvar_cadastro(nome, email, senha):
         #verificando função 
         print(f"dados salvos: {nome}, {email},{senha}")
 
+
+# rota /cadastro
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
-# pagina de cadastro
-
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro_post():
@@ -32,6 +31,28 @@ def cadastro_post():
         salvar_cadastro(nome, email, senha)
         return render_template('reservas.html')
 
+
+@app.route('/logar', methods=['GET','POST'])
+# Função para verificar se o usuário está registrado no CSV
+def logar():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form ['senha']
+
+        with open (cadastro_csv, mode ='r') as arquivo_cadastros:
+            leitor = csv.leitor(arquivo_cadastros)
+
+            for linha in leitor:
+                if linha['email'] == email and linha['senha'] == senha:
+                    return redirect(url_for('sucess'))
+            return "invalido", 401
+    return render_template('cadastrar-sala.html')
+
+
+@app.route('/sucess')
+def sucess():
+    return "LOGIN DEU CERTO!!!!"
+
 # salvar sala digitada pelo usuario no csv 
 def salvar_sala(tipo, capacidade, descricao):
     with open(cadastro_sala_csv, 'a', newline = '') as salas_cadastros:
@@ -40,42 +61,32 @@ def salvar_sala(tipo, capacidade, descricao):
 
         print(f"dados salvos: {tipo}, {capacidade}, {descricao}")
 
-
+# ta errado, detalhe da reserva mostra os dados da sala reservada, horario inicial, final,
 @app.route('/detalhe-reserva', methods=['GET', 'POST'])
 def detalhe_sala():
     if request.method == 'POST':
         tipo = request.form['tipo']
         capacidade = request.form['capacidade'] 
         descricao = request.form['descricao']
+        
+        if tipo == '' or capacidade == '' or descricao == '':
+            return render_template ('cadastrar-sala.html')
         salvar_sala(tipo, capacidade, descricao)
-        return render_template('cadastrar-sala.html')
+        return render_template('listar-sala.html')
 
 
 
 @app.route('/descricao-sala', methods = ['GET', 'POST'])
 
 
+#botao de logout vai pra rota /login  mas nao vai pra nenhum lugar essa rota
 
 # pagina inicial
-@app.route('/')
-def login():
-    return render_template('login.html')
+#@app.route('/')
+#def login():
+#    return render_template('login.html')
 
 #verificação de login
-@app.route('/', methods=['POST'])
-# Função para verificar se o usuário está registrado no CSV
-def logar(email, senha):
-    try: 
-        with open (cadastro_csv, 'r') as arquivo_cadastros:
-            leitor_csv = csv.reader(arquivo_cadastros)
-            for linha in leitor_csv:
-                # linha 1 - email || linha 2
-                if linha[1] == email and linha[2] == senha:
-                    return redirect(url_for('reservas'))
-
-    except FileNotFoundError:
-        return render_template('login.html', error='Email ou senha incorretos.')
-
 
 # ROTAS "VAZIAS"
 @app.route('/cadastrar-sala', methods=['GET', 'POST'])
